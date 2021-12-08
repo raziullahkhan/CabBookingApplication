bfcde6386187a8bc9ceaf43a4bbc6c28b3bc982e
Pinaki198 <89732125+Pinaki198@users.noreply.github.com>
2021-12-08 23:22:40 +0530

    Added Customer exception

---
+++ src/main/java/com/cg/cab_booking_app/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cg.cab_booking_app.exception;
+
+public class CustomerNotFoundException extends Exception{
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+
+}
