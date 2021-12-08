348e7613901150160cfbae68460054f94499cfd9
NaveenNani246 <95739995+NaveenNani246@users.noreply.github.com>
2021-12-08 23:41:05 +0530

    Uploaded TripBooking Exception

---
+++ src/main/java/com/cg/cab_booking_app/exception/TripBookingNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cg.cab_booking_app.exception;
+
+public class TripBookingNotFoundException extends Exception{
+
+	public TripBookingNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+}
