224555eb4a7600351c4712b6285b7ce462bac082
nikhil884 <95769671+nikhil884@users.noreply.github.com>
2021-12-08 23:59:20 +0530

    Uploaded Admin Excepttion

---
+++ src/main/java/com/cg/cab_booking_app/exception/AdminNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cg.cab_booking_app.exception;
+
+public class AdminNotFoundException extends Exception{
+
+	public AdminNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+
+}
